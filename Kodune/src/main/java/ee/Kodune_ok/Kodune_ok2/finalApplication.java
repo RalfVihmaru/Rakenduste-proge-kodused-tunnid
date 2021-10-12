2f0dc1ae3fdf4ba66e8ef67ef2ac5724a036bd79
Ralf Vihmaru <55079802+RalfVihmaru@users.noreply.github.com>
2021-10-12 23:31:34 +0300

    Materjalid ja kodused
    Kodune 3 update tehtud

---
+++ Kodune/src/main/java/ee/Kodune_ok/Kodune_ok2/finalApplication.java
@@ -0,0 +1,13 @@
+package ee.Kodune.ok.Kodune_ok2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class finalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(finalApplication.class, args);
+    }
+
+}
